5fffd1a0af43f9cb80329f25a73ea2c8731d0e16
Vishal TK <vishalsanthosh1999@gmail.com>
2023-04-08 18:18:27 +0530

    fix loading canvas

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,12 +1,9 @@
 import { useRef } from "react"
 import dynamic from "next/dynamic"
-import Link from "next/link"
 import { useSize } from "@chakra-ui/react-use-size"
-import { useScroll } from "framer-motion"
 import { useTheme } from "next-themes"
 import colors from "tailwindcss/colors"
 
-import { socials } from "@/lib/constants"
 import { Logo } from "@/components/Logo"
 
 const FlowField = dynamic(
@@ -19,7 +16,7 @@ export const HomePage = () => {
   const flowFieldRef = useRef<HTMLDivElement>(null)
   const flowFieldDimensions = useSize(flowFieldRef)
 
-  const { setTheme, theme } = useTheme()
+  const { theme } = useTheme()
 
   return (
     <>
@@ -28,22 +25,25 @@ export const HomePage = () => {
         ref={flowFieldRef}
       >
         {flowFieldDimensions && (
-          <FlowField
-            // @ts-ignore
-            className="h-full w-full overflow-hidden rounded-2xl"
-            width={flowFieldDimensions.width}
-            height={flowFieldDimensions.height}
-            bgColor={theme === "dark" ? colors.zinc[100] : colors.zinc[900]}
-            fgColor={
-              theme === "dark"
-                ? colors.zinc[900] + "33"
-                : colors.zinc[100] + "33"
-            }
-          />
+          <>
+            <FlowField
+              // @ts-ignore
+              className="h-full w-full overflow-hidden rounded-2xl"
+              width={flowFieldDimensions.width}
+              height={flowFieldDimensions.height}
+              bgColor={theme === "dark" ? colors.zinc[100] : colors.zinc[900]}
+              fgColor={
+                theme === "dark"
+                  ? colors.zinc[900] + "33"
+                  : colors.zinc[100] + "33"
+              }
+            />
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-125">
+              <Logo theme={theme === "dark" ? "light" : "dark"} />
+            </div>
+          </>
         )}
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-125">
-          <Logo theme={theme === "dark" ? "light" : "dark"} />
-        </div>
+
         <p className="mt-2 text-right text-base text-zinc-400">by vishal tk</p>
       </section>
       <section className="mt-44 text-center text-lg font-medium opacity-75">
